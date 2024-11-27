9e28e7927ad08440390bb86c4fe04e941a6af8be
Kanan.Mammadov <Kanan.Mammadov@AZLOTO_217>
2024-11-27 16:35:04 +0400

    Add project files.

---
+++ Project1_AdonetCustomerr/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Project1_AdonetCustomerr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnList = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCityİd = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtCityCountry = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCityName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnList
+            // 
+            this.btnList.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnList.Location = new System.Drawing.Point(151, 192);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(285, 41);
+            this.btnList.TabIndex = 0;
+            this.btnList.Text = "Çeşidlə";
+            this.btnList.UseVisualStyleBackColor = false;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(73, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Şəhər İd";
+            // 
+            // txtCityİd
+            // 
+            this.txtCityİd.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtCityİd.Location = new System.Drawing.Point(172, 53);
+            this.txtCityİd.Name = "txtCityİd";
+            this.txtCityİd.Size = new System.Drawing.Size(247, 22);
+            this.txtCityİd.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.InactiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(480, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(735, 422);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // txtCityCountry
+            // 
+            this.txtCityCountry.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtCityCountry.Location = new System.Drawing.Point(172, 112);
+            this.txtCityCountry.Name = "txtCityCountry";
+            this.txtCityCountry.Size = new System.Drawing.Size(247, 22);
+            this.txtCityCountry.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(105, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ölkə";
+            // 
+            // txtCityName
+            // 
+            this.txtCityName.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtCityName.Location = new System.Drawing.Point(172, 84);
+            this.txtCityName.Name = "txtCityName";
+            this.txtCityName.Size = new System.Drawing.Size(247, 22);
+            this.txtCityName.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(61, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Şəhər Adı";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnCreate.Location = new System.Drawing.Point(151, 253);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(285, 41);
+            this.btnCreate.TabIndex = 10;
+            this.btnCreate.Text = "Əlavə et";
+            this.btnCreate.UseVisualStyleBackColor = false;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnUpdate.Location = new System.Drawing.Point(151, 379);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(285, 41);
+            this.btnUpdate.TabIndex = 12;
+            this.btnUpdate.Text = "Güncəllə";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnDelete.Location = new System.Drawing.Point(151, 318);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(285, 41);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btnSearch.Location = new System.Drawing.Point(151, 434);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(285, 41);
+            this.btnSearch.TabIndex = 13;
+            this.btnSearch.Text = "Axtar";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1243, 587);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txtCityName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtCityCountry);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtCityİd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnList);
+            this.Name = "Form1";
+            this.Text = "City Process Formm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCityİd;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtCityCountry;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCityName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
