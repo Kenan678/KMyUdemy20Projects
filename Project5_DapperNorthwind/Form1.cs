03b272c2e9861049350686884d9737f82b140f85
Kanan Mammadov <khanan.mammadov@khazar.org>
2025-02-05 12:40:36 +0400

    cm5

--- Project5_DapperNorthwind/Form1.cs
+++ Project5_DapperNorthwind/Form1.cs
@@ -16,5 +16,10 @@ namespace Project5_DapperNorthwind
         {
             InitializeComponent();
         }
+
+        private   void btnCategoryList_Click(object sender, EventArgs e)
+        {
+            string query =""
+        }
     }
 }
--- Project5_DapperNorthwind/Repositories/CateogyRepositories/ICategoryRepository.cs
+++ Project5_DapperNorthwind/Repositories/CateogyRepositories/ICategoryRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Project5_DapperNorthwind.Dtos.CategoryDtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace Project5_DapperNorthwind.Repositories.CateogyRepositories
 {
     public interface ICategoryRepository
     {
+        Task<List<ResultCategoryDto>> GetAllCategoryAsync();
+        Task CreateCategoryAsync(CreateCategoryDto createCategoryDto);  
+        Task UpdateCategoryAsync(UpdateCategoryDto updateCategoryDto);
+        Task DeleteCategoryAsync(int id);
+        Task<GetByIdCategoryDto> GetByIdCategoryAsync(int id);
     }
 }
