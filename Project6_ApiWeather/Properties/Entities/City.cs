d7bf65a4cad0715ea0c790e61d1d5615feef4621
Kanan Mammadov <khanan.mammadov@khazar.org>
2025-02-06 09:50:24 +0400

    cm414

---
+++ Project6_ApiWeather/Properties/Entities/City.cs
@@ -0,0 +1,17 @@
+﻿namespace Project6_ApiWeather.Properties.Entities
+{
+    public class City
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public string Country { get; set; }
+        public decimal Temp { get; set; }
+        public string Detail { get; set; }
+
+
+
+
+
+
+    }
+}
