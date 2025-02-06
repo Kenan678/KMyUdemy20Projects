32841ec230446ad7f94f084f1772ad8c0c9dae49
Kanan Mammadov <khanan.mammadov@khazar.org>
2025-02-06 15:55:04 +0400

    hh

---
+++ Project6_ApiWeather/Properties/Context/WheatherContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project6_ApiWeather.Properties.Context
+{
+    public class WheatherContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+        }
+    }
+}
