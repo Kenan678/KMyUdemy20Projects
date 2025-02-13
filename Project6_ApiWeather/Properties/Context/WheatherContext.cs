bb33a572d6eb9d427e6a1d1a86d55b102fe5e7d6
Kanan Mammadov <khanan.mammadov@khazar.org>
2025-02-13 11:29:09 +0400

    Migrations

--- Project6_ApiWeather/Properties/Context/WheatherContext.cs
+++ Project6_ApiWeather/Properties/Context/WheatherContext.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Project6_ApiWeather.Properties.Entities;
 
 namespace Project6_ApiWeather.Properties.Context
 {
-    public class WheatherContext:DbContext
+    public class WheatherContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer();k
+            optionsBuilder.UseSqlServer("Server=AZLOTO_217\\SQLEXPRESS;initial catalog=Db5Project20;Integrated Security=True;");
+
 
         }
+        public DbSet<City> Cities { get; set; }
     }
 }
