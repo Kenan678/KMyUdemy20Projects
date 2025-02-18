fb803d882032f09dd31b056d1eb9fa2f633978eb
Kanan Mammadov <khanan.mammadov@khazar.org>
2025-02-18 17:27:54 +0400

    cm46

--- Project6_ApiWeather/Properties/Entities/City.cs
+++ Project6_ApiWeather/Properties/Entities/City.cs
@@ -1,17 +1,15 @@
-﻿namespace Project6_ApiWeather.Properties.Entities
+﻿using System.Text.Json.Serialization;
+
+namespace Project6_ApiWeather.Properties.Entities
 {
     public class City
     {
+       
         public int CityId { get; set; }
         public string CityName { get; set; }
         public string Country { get; set; }
         public decimal Temp { get; set; }
         public string Detail { get; set; }
-
-
-
-
-
 
     }
 }
