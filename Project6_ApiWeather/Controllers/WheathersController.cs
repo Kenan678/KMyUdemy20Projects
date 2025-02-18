0480177a2e6c2ddae7e9efd6674202288d119319
Kanan Mammadov <khanan.mammadov@khazar.org>
2025-02-18 14:09:42 +0400

    cmm

---
+++ Project6_ApiWeather/Controllers/WheathersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project6_ApiWeather.Properties.Context;
+
+namespace Project6_ApiWeather.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WheathersController : ControllerBase
+    {
+        WheatherContext context=new WheatherContext();
+        [HttpGet]
+        public IActionResult wh
+    }
+}
