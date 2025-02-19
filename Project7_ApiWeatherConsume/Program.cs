601c9b48d848a9a8ef221698e10730266d685d78
Kanan Mammadov <khanan.mammadov@khazar.org>
2025-02-19 13:02:02 +0400

    Console Screen maked

---
+++ Project7_ApiWeatherConsume/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Xml;
+
+Console.WriteLine("Api Consume Welcome");
+Console.WriteLine();
+Console.WriteLine("#### Select the Things ####");
+Console.WriteLine();
+Console.WriteLine("1-Get The City List ");
+Console.WriteLine("2-Add The New City");
+Console.WriteLine("3-Delete The City ");
+Console.WriteLine("4-Update The City ");
+Console.WriteLine("5-Get By Id to City");
+Console.WriteLine();
+string number;
+
+Console.WriteLine("Select: ");
+number = Console.ReadLine();
+if (number == "1")
+{
+    Console.WriteLine("City List Coiming:");
+}
+if (number == "2")
+{
+    Console.WriteLine(" New City Added side:");
+}
+if (number == "3")
+{
+    Console.WriteLine("City Deleted side :");
+}
+
+
+
