a2d22b4a47a609c36801754ef53cbd88a3ab3687
Kanan Mammadov <khanan.mammadov@khazar.org>
2025-02-19 14:48:55 +0400

    getconsume

--- Project7_ApiWeatherConsume/Program.cs
+++ Project7_ApiWeatherConsume/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Xml;
+﻿using Newtonsoft.Json.Linq;
+using System.Text.Json.Nodes;
+using System.Xml;
 
 Console.WriteLine("Api Consume Welcome");
 Console.WriteLine();
@@ -16,16 +18,33 @@ Console.WriteLine("Select: ");
 number = Console.ReadLine();
 if (number == "1")
 {
-    Console.WriteLine("City List Coiming:");
+    string url = "https://localhost:7063/api/Wheathers";
+    using(HttpClient client = new HttpClient())
+    {
+        HttpResponseMessage response = await client.GetAsync(url);
+        string responseBody = await response.Content.ReadAsStringAsync();
+        JArray jArray=JArray.Parse(responseBody);   
+        foreach(var item in jArray)
+        {
+            string cityName = item["cityName"].ToString();
+            Console.WriteLine($"City:{cityName}");
+        }
+    }
+
+
+
 }
 if (number == "2")
 {
+https://localhost:7063/api/Wheathers
     Console.WriteLine(" New City Added side:");
 }
 if (number == "3")
 {
+https://localhost:7063/api/Wheathers
     Console.WriteLine("City Deleted side :");
 }
+Console.Read();
 
 
 
