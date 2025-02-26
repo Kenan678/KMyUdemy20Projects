1194c413d9370859b47cbb8d8b9532beae7184d1
Kanan Mammadov <khanan.mammadov@khazar.org>
2025-02-26 10:13:57 +0400

    Consume

--- Project7_ApiWeatherConsume/Program.cs
+++ Project7_ApiWeatherConsume/Program.cs
@@ -8,9 +8,10 @@ Console.WriteLine("#### Select the Things ####");
 Console.WriteLine();
 Console.WriteLine("1-Get The City List ");
 Console.WriteLine("2-City and Weather Tempurature List");
-Console.WriteLine("3-Delete The City ");
-Console.WriteLine("4-Update The City ");
-Console.WriteLine("5-Get By Id to City");
+Console.WriteLine("3-Add The City ");
+Console.WriteLine("4-Delete The City ");
+Console.WriteLine("5-Update The City ");
+Console.WriteLine("6-Get By Id to City");
 Console.WriteLine();
 string number;
 
@@ -19,12 +20,12 @@ number = Console.ReadLine();
 if (number == "1")
 {
     string url = "https://localhost:7063/api/Wheathers";
-    using(HttpClient client = new HttpClient())
+    using (HttpClient client = new HttpClient())
     {
         HttpResponseMessage response = await client.GetAsync(url);
         string responseBody = await response.Content.ReadAsStringAsync();
-        JArray jArray=JArray.Parse(responseBody);   
-        foreach(var item in jArray)
+        JArray jArray = JArray.Parse(responseBody);
+        foreach (var item in jArray)
         {
             string cityName = item["cityName"].ToString();
             Console.WriteLine($"City:{cityName}");
@@ -52,14 +53,21 @@ if (number == "2")
 
 
         }
-        if (number == "3")
-        {
-        https://localhost:7063/api/Wheathers
-            Console.WriteLine("City Deleted side :");
-        }
-        Console.Read();
     }
+    
 }
+if (number == "3")
+{
+    string url = "https://localhost:7063/api/Wheathers";
+    var newWeatherCity = new
+    {
+
+    }
+
+}
+Console.Read();
+   
+
 
 
 
