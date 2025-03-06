f3597678ba59c060698fab308442f265d24d8f90
Kanan Mammadov <khanan.mammadov@khazar.org>
2025-03-06 18:06:19 +0400

    t

--- Project4_EntityFrameworkCodeFirstMovie/DAL/Context/MovieContext.cs
+++ Project4_EntityFrameworkCodeFirstMovie/DAL/Context/MovieContext.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Project4_EntityFrameworkCodeFirstMovie.DAL.Context
 {
-    public class MovieContext:DbContext
+    public class MovieContext:DbContext 
     { 
         public DbSet<Movie> Movies { get; set; } 
         public DbSet<Category> Categories { get; set; }
